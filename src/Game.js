91dd5787d8561cdbfab257a4495950261cf7675d
Isak <kallenbach13@gmail.com>
2020-06-12 15:02:03 +0100

    games sort alphabetically and game link opens in new tab

--- src/Game.js
+++ src/Game.js
@@ -5,7 +5,7 @@ const Game = (props) => {
 return (
     <div className="ui column">
     <div className="ui card">
-        <img class="image-item" alt={props.game.title} src={props.game.image + ".jpg"} onClick={(e) => {e.preventDefault(); window.location.href= props.game.url}}/>
+        <img class="image-item" alt={props.game.title} src={props.game.image + ".jpg"} onClick={()=> window.open(props.game.url, "_blank")}/>
         <h4>{props.game.title}</h4>
         <p>Developer: {props.game.developer}</p>
         <p>Publisher: {props.game.publisher}</p>
